dcfe3343c926a5f2ad164f20c32c4b5974c66787
SarthKanyal <sarthak.kanyal@alumni.ubc.ca>
2022-01-14 00:12:50 -0500

    renderbatch implementing

--- src/main/java/render/RenderBatch.java
+++ src/main/java/render/RenderBatch.java
@@ -1,10 +1,11 @@
 package render;
 
 import components.SpriteRenderer;
+import flame.Window;
+import org.joml.Vector4f;
 
 import static org.lwjgl.opengl.GL15.*;
-import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
-import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
+import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL30.glBindVertexArray;
 import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 
@@ -68,7 +69,7 @@ public class RenderBatch {
 
     }
 
-    private int generateIndices(){
+    private int[] generateIndices(){
 
         //we need 6 vertices per sprite ie 3 vertices in upper triangle and 3 in the lower triangle.
         int[] elements = new int[maxBatchSize*6];
@@ -76,14 +77,71 @@ public class RenderBatch {
         for(int i=0; i<maxBatchSize; i++){
             loadElementIndices(elements,i);
         }
-
-
-
-        return 0;
+        return elements;
     }
 
     private void loadElementIndices(int[] elements, int index){
-        int offSetArray = 6;
+        int offSetArray = index*6;
+        int offset = index*4;
+        //example quads: 320021 764465
+        //drawing the upper triangle of the quad
+        elements[offSetArray] = offset+3;
+        elements[offSetArray+1] = offset+2;
+        elements[offSetArray+2] = offset;
+
+        //drawing the lower triangle of the quad
+        elements[offSetArray+3] = offset;
+        elements[offSetArray+4] = offset+2;
+        elements[offSetArray+5] = offset+1;
+
+    }
+
+    //imp to review
+    private void render(){
+
+        glBindBuffer(GL_ARRAY_BUFFER,vboID);
+        glBufferSubData(GL_ELEMENT_ARRAY_BUFFER,0,vertices);
+
+        //using the shader
+        shader.use();
+        shader.uploadMat4("uProjection", Window.getCurrentScene().getCamera().getProjectionMatrix());
+        shader.uploadMat4("uView",Window.getCurrentScene().getCamera().getViewMatrix());
+
+        glBindVertexArray(vaoID);
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(0);
+
+        glDrawElements(GL_TRIANGLES,this.numSprites*6, GL_UNSIGNED_INT, 0);
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glBindVertexArray(0);
+
+        shader.detach();
+    }
+
+    public void addSprite(SpriteRenderer sr){
+        //add sprite
+
+        int index = this.numSprites;
+        this.sprites[index] = sr;
+        this.numSprites++;
+
+        //add properties to local vertex arrray
+
+        loadVertexProperties(index);
+        if(this.numSprites>=maxBatchSize){
+            this.vacancy = false;
+        }
+
+    }
+
+    private void loadVertexProperties(int index) {
+        SpriteRenderer spr = this.sprites[index];
+
+        int offset = index*4*vertex_size;
+
+        Vector4f color = spr.getColor();
+
     }
 
 
